63c83b80bad327dd9efc66277278f452dec5f206
GMKBabu <babu.g0730@gmail.com>
2018-09-13 20:46:40 +0530

    Update MySQLConnUtils.java

--- src/com/dev/tourguide/MySQLConnUtils.java
+++ src/com/dev/tourguide/MySQLConnUtils.java
@@ -8,7 +8,7 @@ public class MySQLConnUtils {
   
  public static Connection getMySQLConnection()
          throws ClassNotFoundException, SQLException {
-     String hostName = "localhost";
+     String hostName = "54.85.44.143";
      String dbName = "tourguide";
      String userName = "root";
      String password = "password123";
